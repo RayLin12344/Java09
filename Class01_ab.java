8b9a3225faec499453158f206cd6cdaac6ff961f
RayLin12344 <93502719+RayLin12344@users.noreply.github.com>
2022-01-17 10:59:08 +0800

    Create Class01_ab.java

---
+++ Class01_ab.java
@@ -0,0 +1,16 @@
+package Java09;
+class CRectangle
+{
+	int width;
+	int height;
+	public CRectangle(int w,int h)	//(a)
+	{
+		width=w;
+		height=h;
+	}
+	public CRectangle()	//(b)
+	{
+		width=10;
+		height=8;
+	}
+}
